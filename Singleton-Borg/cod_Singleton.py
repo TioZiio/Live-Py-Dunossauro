9689df9b5608d446dced757359430af7d8496499
TioZiio <davidmaf@gmail.com>
2024-10-03 12:57:30 -0300

    Conceitos iniciais para o Singleton

---
+++ Singleton-Borg/cod_Singleton.py
@@ -0,0 +1,9 @@
+class Singleton:
+    instance: Singleton
+        
+    @classmethod
+    def get_instance(cls) -> Singleton:
+        if not hasattr(cls, 'instance'):
+            cls.instance = Singleton()
+        return cls.instance
+
